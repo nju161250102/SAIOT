3621de3fd04f4bd24388b1b0203ea318fa1a1dab
Adian <QRXwzx@Outlook.com>
2021-04-04 10:26:14 +0800

    新增：由../saiot.db自动生成的数据

---
+++ api/models.py
@@ -0,0 +1,57 @@
+from peewee import *
+
+database = SqliteDatabase('../saiot.db')
+
+
+class UnknownField(object):
+    def __init__(self, *_, **__): pass
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Connection(BaseModel):
+    device_id = IntegerField(column_name='deviceId')
+    topic = TextField(null=True)
+
+    class Meta:
+        table_name = 'connection'
+
+
+class Device(BaseModel):
+    decode = TextField(null=True)
+    description = TextField(null=True)
+    ip = TextField(null=True)
+    name = TextField(null=True)
+    port = IntegerField(null=True)
+    secret = TextField(null=True)
+    type = TextField(null=True)
+
+    class Meta:
+        table_name = 'device'
+
+
+class Rule(BaseModel):
+    columns = TextField(null=True)
+    condition = TextField(null=True)
+    description = TextField(null=True)
+    device_id = IntegerField(column_name='deviceId')
+    name = TextField(null=True)
+    path = TextField(null=True)
+    status = IntegerField(constraints=[SQL("DEFAULT 0")], null=True)
+    topic = TextField(null=True)
+
+    class Meta:
+        table_name = 'rule'
+
+
+class SqliteSequence(BaseModel):
+    name = BareField(null=True)
+    seq = BareField(null=True)
+
+    class Meta:
+        table_name = 'sqlite_sequence'
+        primary_key = False
+
